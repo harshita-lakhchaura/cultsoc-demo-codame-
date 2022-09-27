21cc03185dc8d7145dcf69d3e5b4a9b5b2f283ae
Harshita <harshitalakhchaura@gmail.com>
2022-09-27 20:32:42 +0530

    Last commit with responsive

--- src/components/Groups.js
+++ src/components/Groups.js
@@ -10,28 +10,28 @@ function Groups() {
                     <h3 class="section-subheading text-muted">The subsets of our society</h3>
                 </div>
                 <div class="texts-center">
-                    <div class="col-md-4">
+                    <div class="col-md-4" id="group">
                         <span class="fa-stack fa-4x">
                             <img src="../assets/download.jpg" alt="dance" height="150px"></img>
                         </span>
                         <h2 class="my-3">Dance CLub</h2>
                         <p class="text-muted">Get mesmerised by the enthralling footwork of our dancers</p>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4" id="group">
                         <span class="fa-stack fa-4x">
                             <img src="../assets/music.jpg" height="145px"></img>
                         </span>
                         <h2 class="my-3">Music Club</h2>
                         <p class="text-muted">Come and get lost in the mellifluous voices of our talented musicians</p>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4" id="group">
                         <span class="fa-stack fa-4x">
                             <img src="../assets/drama.png" height="150px"></img>
                         </span>
                         <h2 class="my-3">Theatre</h2>
                         <p class="text-muted">You will get moved performance by the gifted actors of our institute</p>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4" id="group">
                         <span class="fa-stack fa-4x">
                             <img src="../assets/literature.png" alt="dance" height="150px"></img>
                         </span>
--- src/components/Team.js
+++ src/components/Team.js
@@ -11,7 +11,7 @@ export default function Team() {
             <div class="container" data-aos="fade-up" data-aos-delay="200" data-aos-once="true">
                 <div class="text-center">
                     <h2 class="section-heading text-uppercase">Our Crew</h2>
-                    <h3 class="section-subheading text-muted">Let's introduce you to the hardworking and brilliant team working day and night behind the progress of Cult-Soc</h3>
+                    <h3 class="section-subheading text-muted" id="subheading">Let's introduce you to the hardworking and brilliant team working day and night behind the progress of Cult-Soc</h3>
                 </div>
                 <div class="row">
                     <div class="col-lg-4">
